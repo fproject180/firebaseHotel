1c8ffccc8c8da6efa322c44ae71398a8d5874408
SamarthDambalkar7 <Samarthdambalkar7@gmail.com>
2021-06-07 18:12:40 +0530

    services Page added

--- lib/components/bottomNavbarconfig.dart
+++ lib/components/bottomNavbarconfig.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hotel/pages/billingPage.dart';
 import 'package:hotel/pages/ratingPage.dart';
+import 'package:hotel/pages/servicesPage.dart';
 
 class BottomNavConfig extends StatefulWidget {
   @override
@@ -16,20 +17,24 @@ class _BottomNavConfigState extends State<BottomNavConfig> {
       backgroundColor: Colors.purple[400],
       items: [
         TabItem(icon: Icons.home, title: 'Home'),
-        TabItem(icon: Icons.map, title: 'Discovery'),
-        TabItem(icon: Icons.add, title: 'Add'),
+        TabItem(icon: Icons.room_service_outlined, title: 'Services'),
+        TabItem(icon: Icons.explore_outlined, title: 'Navigate'),
         TabItem(icon: Icons.star_rate_outlined, title: 'Rate Us'),
         TabItem(icon: Icons.payments_outlined, title: 'Payment'),
       ],
       initialActiveIndex: 2,
       onTap: (index) {
-        if (index == 4) {
+        if (index == 1) {
           Navigator.of(context)
-              .push(CupertinoPageRoute(builder: (context) => billingPage()));
+              .push(CupertinoPageRoute(builder: (context) => ServicesPage()));
         }
         if (index == 3) {
           Navigator.of(context)
               .push(CupertinoPageRoute(builder: (context) => RatingPage()));
+        }
+        if (index == 4) {
+          Navigator.of(context)
+              .push(CupertinoPageRoute(builder: (context) => billingPage()));
         }
       },
     );
